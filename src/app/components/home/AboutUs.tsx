a819005a386ae8834643d0812d8d67d53e19c601
Doğukan Topçu <dogukantopcu35@gmail.com>
2024-06-21 18:08:02 +0300

    about us responsive

--- src/app/components/home/AboutUs.tsx
+++ src/app/components/home/AboutUs.tsx
@@ -5,20 +5,20 @@ import { useState } from 'react';
 const AboutUs = () => {
     const data = aboutUsData.filter((x) => x.isActive)[0];
   return (
-    <section className='container max-w-7xl mx-auto py-24 flex flex-col gap-16'>
+    <section className='container max-w-7xl mx-auto sm:py-24 py-16 flex flex-col sm:gap-16 gap-10 px-6'>
         {/* title */}
-        <h1 className='text-2xl tracking-[0.3em] text-[#25456c]'>{data.title}</h1>
+        <h1 className='sm:text-2xl text-lg tracking-[0.3em] text-[#25456c]'>{data.title}</h1>
 
         {/* Content */}
-        <div className='grid grid-cols-2'>
-            <h2 className='text-5xl font-bold tracking-wide leading-snug'>{data.subtitle}</h2>
-            <desc className='font-semibold flex items-center justify-center'>
+        <div className='sm:grid sm:grid-cols-2'>
+            <h2 className='sm:text-5xl text-2xl font-bold sm:tracking-wide leading-snug sm:text-left text-center sm:mb-0 mb-8'>{data.subtitle}</h2>
+            <desc className='sm:font-semibold sm:text-md text-sm flex items-center justify-center sem:text-left text-center'>
                 {data.desc}
             </desc>
         </div>
 
         {/* Data */}
-        <div className='grid grid-cols-3 mt-20 gap-y-20'>
+        <div className='grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:mt-20 mt-8 gap-y-20'>
             {
                 data.numerics.map((d, idx) => {
                     return (
@@ -45,7 +45,7 @@ interface DataCardInterface {
 const DataCard = ( { data } : { data : DataCardInterface} ) => {
     
     return (
-        <div className='flex flex-col items-center justify-center gap-10'>
+        <div className='flex flex-col items-center justify-center sm:gap-10 gap-6'>
             <motion.div
 
                 style={{
@@ -53,9 +53,9 @@ const DataCard = ( { data } : { data : DataCardInterface} ) => {
                     backgroundSize: "cover",
                     backgroundPosition: "center",
                 }}
-                className='w-36 h-36'
+                className='sm:w-36 sm:h-36 w-24 h-24'
             />
-            <div className='flex flex-col justify-center items-center gap-4'>
+            <div className='flex flex-col justify-center items-center sm:gap-4 gap-2'>
                 <p>{data.toptext}</p>
                 <div className='flex justify-center items-end gap-3'>
                     <p className='text-4xl font-bold tracking-tighter'>{data.num.toLocaleString('en', {useGrouping:true})}</p>
@@ -74,7 +74,7 @@ const References = () => {
         <div className='flex flex-col items-center justify-center pt-10'>
             <h1 className='text-[#25456c] text-2xl tracking-wider font-bold'>REFERENCES</h1>
 
-            <div className='flex justify-center items-center gap-x-10 pt-12'>
+            <div className='flex justify-center items-center flex-wrap gap-x-10 pt-12'>
                 {
                     references.map((r, idx) => {
                         return (
@@ -107,7 +107,7 @@ const ReferenceCard = ( { reference } : { reference : ReferenceCardInterface }) 
                     backgroundRepeat: "no-repeat",
                     backgroundPosition: "center",
                 }}
-                className={`w-32 h-32 transition-all duration-300 ease ${isHovered ? "-translate-y-4" : "grayscale"}`}
+                className={`sm:w-32 sm:h-32 w-16 h-16 transition-all duration-300 ease ${isHovered ? "-translate-y-4" : "grayscale"}`}
             />
 
             <p className={`transition-all duration-300 ease ${isHovered ? "opacity-100" : "opacity-0"}`}>
